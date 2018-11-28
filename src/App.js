8daa63cc6da0bead6135284d6dc5c7eb8d917ebe
hades <quentindevial19@gmail.com>
2018-11-28 11:12:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Header, MovieList, MovieDetails } from './components';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App d-flex flex-column">
+        <Header />
+        <div className="d-flex flex-row flex-fill pt-4 p-2" >
+          <MovieList />
+          <MovieDetails />
+        </div>
       </div>
     );
   }
