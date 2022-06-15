26d427fd129e172aa1a32e5faa294328d7aa6e65
Tsiky <tsiky@test.com>
2022-06-15 15:18:36 +0300

    IMP prepare Components

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,1 +1,4 @@
 export {default as Header} from './Header/header'
+export {default as MovieDetails} from './MovieDetails/movieDetails'
+export {default as MovieList} from './MovieList/movieList'
+export {default as MovieElement} from './MovieList/movieElement'
