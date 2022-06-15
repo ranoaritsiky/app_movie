b4f459a89f79d88e17e8332b71f25f63b2926d7c
Tsiky <tsiky@test.com>
2022-06-15 16:28:19 +0300

    IMP using props to moviedetails

--- src/Components/MovieDetails/movieDetails.js
+++ src/Components/MovieDetails/movieDetails.js
@@ -4,7 +4,19 @@ export default class MovieDetails extends Component {
     render(){
         return(
             <div className="w-25">
-                Movie details
+                <div className="d-flex flex-column">
+                    <h3>{this.props.movieElement.title}</h3>
+                    <hr/>
+                    <img src={this.props.movieElement.img} alt="myimage" style={{'max-height':'15rem'}}/>
+                    <hr/>
+                    <span>
+                        {this.props.movieElement.details}
+                    </span>
+                    <hr/>
+                    <p>
+                        {this.props.movieElement.description}
+                    </p>
+                </div>
             </div>
         )
     }
