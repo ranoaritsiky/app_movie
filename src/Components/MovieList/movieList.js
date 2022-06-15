f0efb87ff683c22c55492b978d6b7e9077e3f46d
Tsiky <tsiky@test.com>
2022-06-15 15:28:50 +0300

    IMP formatting style bootstrap

--- src/Components/MovieList/movieList.js
+++ src/Components/MovieList/movieList.js
@@ -3,7 +3,7 @@ import React, { Component} from 'react';
 export default class MovieList extends Component {
     render(){
         return(
-            <div>
+            <div className="border w-75">
                 Movie List
             </div>
         )
