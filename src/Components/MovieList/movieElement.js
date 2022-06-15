5820f9dd281603b50977686e363b15e31aaa7e2d
Tsiky <tsiky@test.com>
2022-06-15 16:12:42 +0300

    IMP create card

--- src/Components/MovieList/movieElement.js
+++ src/Components/MovieList/movieElement.js
@@ -2,17 +2,18 @@ import React, { Component} from 'react';
 export default class MovieElement extends Component {
     render(){
         return(
-            <div className="card m-3" style={{width:'18rem'}}>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/f/f8/Logo_du_Programme_des_Leos_Clubs.png" alt="LEO"/>
-                <div className="card-body">
-                    <hr/>
-                    <h5 className="card-title">Title</h5>
-                    <p className="card-text">
-                        Text here
-                    </p>
-                    <a href="#home" className="btn btn-primary">Go somewhere</a>
+            
+            <div className="w-25 p-2">
+                <div className="border d-flex">
+                    <img width="150" height="200" alt="film" src="https://thispersondoesnotexist.com/image" />
+                    <div className="flex-fill d-flex flex-column p3 ">
+                        <h5 className="pl-2 pt-2 m-3">Title</h5>
+                        <hr className="w-100"/>
+                        <p className="card-text p-2">
+                            details
+                        </p>
+                    </div>
                 </div>
-
             </div>
         )
     }
