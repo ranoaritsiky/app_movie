af740801253689e8f5beadeb6152c76ccb3bb723
Tsiky <tsiky@test.com>
2022-06-15 15:28:17 +0300

    IMP make formatting

--- src/Components/MovieDetails/movieDetails.js
+++ src/Components/MovieDetails/movieDetails.js
@@ -3,7 +3,7 @@ import React, { Component} from 'react';
 export default class MovieDetails extends Component {
     render(){
         return(
-            <div>
+            <div className="w-25">
                 Movie details
             </div>
         )
